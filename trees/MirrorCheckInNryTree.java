80f642959340e425c15ca6dd8369c38d16cac794
vamsivardhanreddy77 <vamsireddynani328@gmail.com>
2025-10-16 19:55:00 +0530

    Mirror Check In n- ry tree

---
+++ trees/MirrorCheckInNryTree.java
@@ -0,0 +1,26 @@
+package trees;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class MirrorCheckInNryTree {
+    static int checkMirrorTree(int n, int e, int[] A, int[] B) {
+
+        HashMap<Integer, Stack<Integer>> mapA = new HashMap<>();
+        for (int i = 0; i < 2 * e; i += 2) {
+            int parent = A[i];
+            int child = A[i + 1];
+            mapA.computeIfAbsent(parent, k -> new Stack<>()).push(child);
+        }
+        for (int i = 0; i < 2 * e; i += 2) {
+            int parent = B[i];
+            int child = B[i + 1];
+            if (mapA.get(parent).isEmpty() || !mapA.containsKey(parent) || mapA.get(parent).peek() != child) {
+                return 0;
+            }
+            mapA.get(parent).pop();
+
+        }
+        return 1;
+    }
+}
