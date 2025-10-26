0657e2b2b47da579498a901c44e96af4089fb056
vamsivardhanreddy77 <vamsireddynani328@gmail.com>
2025-10-26 19:36:55 +0530

    Reversing a stack and inserting an element at bottom using recursion

---
+++ stacksandqueues/InsertingElementAtBottom.java
@@ -0,0 +1,14 @@
+package stacksandqueues;
+import java.util.*;
+public class InsertingElementAtBottom {
+    public static Stack<Integer> insertAtBottom(Stack<Integer> st, int x) {
+        if(st.isEmpty()) {
+            st.push(x);
+            return st;
+        }
+        int top=st.pop();
+        insertAtBottom(st,x);
+        st.push(top);
+        return st;
+    }
+}
---
+++ stacksandqueues/ReversingAStack.java
@@ -0,0 +1,12 @@
+package stacksandqueues;
+
+public class ReversingAStack {
+    public static void reverseStack(Stack<Integer> st) {
+        Stack<Integer> res=new Stack<>();
+        while(!st.isEmpty()){
+            int top=st.pop();
+            res.push(top);
+        }
+        st.addAll(res);
+    }
+}
