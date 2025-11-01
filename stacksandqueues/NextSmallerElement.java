5e5aa3f642062e0834539283fbef7da4909fdc3f
vamsivardhanreddy77 <vamsireddynani328@gmail.com>
2025-11-01 19:30:09 +0530

    Finding next greater element in an array using stack

---
+++ stacksandqueues/NextSmallerElement.java
@@ -0,0 +1,25 @@
+//GFG link for this problem https://www.geeksforgeeks.org/dsa/next-smaller-element/
+
+package stacksandqueues;
+import java.util.*;
+public class NextSmallerElement {
+    static ArrayList<Integer> nextSmallerEle(int[] arr) {
+        int n = arr.length;
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) result.add(-1);
+
+        Stack<Integer> st = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() >= arr[i]) {
+                st.pop();
+            }
+            if (!st.isEmpty()) {
+                result.set(i, st.peek());
+            }
+            st.push(arr[i]);
+        }
+
+        return result;
+    }
+}
+
